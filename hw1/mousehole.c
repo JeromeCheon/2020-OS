665eba74694513696d1536b5267eedb352884496
JeromeCheon <21400749@handong.edu>
2020-04-13 16:39:01 +0900

    I need to do on the Mac

--- hw1/mousehole.c
+++ hw1/mousehole.c
@@ -25,10 +25,25 @@
 
 
 MODULE LICENSE("GPL") ;
-
+// This pointer might be necessary later, so let's move on
 char * text = 0x0 ;
+// At this point, I have to replace my own function instead of original sys_open function. 
 
+asmlinkage int (*orig_sys_open)(const char __user * filename, int flags, umode_t mode) ;
+
+asmlinkage int mousehole_sys_open(const char __user * filename, int flags, umode_t mode){
+	char fname[256];
+
+	copy_from_user(fname, filename, 256) ;
+
+	if(filepath[0] != 0x0 && strcmp(filepath, fname) == 0){
+		count++;
+	}
+	return orig_sys_open(filename, flags, mode) ;
+}
+}
 static int hole_open(struct inode *inode, struct file *file){
+
 	char buf[256] ;
 
 	struct tast_struct * t;
@@ -59,13 +74,12 @@ static int hole_release(struct inode *inode, struct file *file){
 
 static ssize_t hole_read(struct file *file, char __user *ubuf, size_t size, loff_t *offset){
 	// ubuf will be less than the size
+	char buf[256] ;
 	ssize_t toread ;
-	if(strlen(text) >= *offset + size) {
-		toread = size ;
-	}
-	else {
-		toread = strlen(text) - *offset ;
-	}
+
+	sprintf(buf,"%s :%d\n", filepath, count) ;
+
+	toread = strlen(buf) >= *offset + size ? size : strlen(buf) - *offset ;
 
 	if (copy_to_user(ubuf, text + *offset, toread))
 		return -EFAULT ;
@@ -74,6 +88,7 @@ static ssize_t hole_read(struct file *file, char __user *ubuf, size_t size, loff
 
 	return toread ;
 }
+// Writing is unnecessary here 
 static ssize_t hole_write(struct file *file, const char __user *ubuf, size_t size, loff_t *offset){
 	return 0;
 }
@@ -88,14 +103,17 @@ static const struct file_operations hole_fops = {
 	// And I must modify here as well and redefine
 	// Maybe I have to replace sys_kill() here referring syscalls.h
 };
-
+// This part will make the path, /proc/mousehole
 static int __init hole_init(void){
 	proc_creat("mousehole", S_IRUGO |S_IWUGO, NULL, &hole_fops) ;
+	printk("This is for checking if the mousehole LKM was successful to be in the kernel. \n");
+
 	return 0;
 }
 
 static void __exit hole_exit(void){
 	remove_proc_entry("mousehole", NULL) ;
+	printk("Okay. This module has been removed. Thanks.! \n") ;
 }
 
 module_init(hole_init) ;
