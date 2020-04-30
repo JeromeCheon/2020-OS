d3c00ab32b29d96694237ce8bef5399a3a020510
Jerome <21400749@handong.edu>
2020-04-30 19:32:23 +0900

    hw2 incompleted

--- hw2/ptsp.c
+++ hw2/ptsp.c
@@ -1,27 +1,24 @@
-
-/* TSP Problem using MultiProcessing */
-/* To build the code, I have to use process creation, join, pipe, signal concepts properly. */
-
 #include <stdio.h>
-#include <stdlib.h>  // for using malloc function
+#include <stdlib.h>
 #include <signal.h>
-#include <unistd.h> // this header is for using execl function.
+#include <unistd.h>
 #include <errno.h>
 #include <string.h>
-#include <fcntl.h>
 #include <sys/types.h> 
-#include <sys/wait.h> // for wait funtion to prevent terminating children process 
+#include <sys/wait.h>
 
-#define MAXSIZE 50
+#define MAXSIZE 50 // Maximum city number
 
 int **route = {0,} ; // I need to allocate width and height dynamically using malloc. 
 int path[MAXSIZE] ;
 int covered[MAXSIZE] ;
-long tot_weight = 0L ;
+int tot_weight = 0 ;
 unsigned long long count = 0ULL ; // At 'count', children will count how many routes they covered.
 int lines = -1 ; // line count from tsp file. 
-long min = -1L ; // Do I have to use malloc to initialize with -1 to give child array?
+long min = -1L ;
 short terminate = 0 ; // Initialize as FALSE 
+int *end_prefix ={ 0x0 } ;
+int prefix_weight =0 ;
 
 void
 parent_handler(int sig){			/* Handler for master process */
@@ -40,72 +37,93 @@ void
 child_handler(int sig){ /* Handler for children processes */
 	int i ;
 	if(sig == SIGINT){
-		terminater = 1 ;
+		terminate = 1 ;
 		exit(0) ;
 	}
 }
-
 void
-children(int prefix, int *p /* pipe file descripter */){
+child_proc(int* prefix, int *p /* pipe file descripter */){
 	signal(SIGINT, child_handler) ;
+    close(p[0]) ;
+    
 
+}
+void
+init_prefix(int** route){
+    /* initialize prefix from the input number of nodes */
+    int i ;
+	for(i=0 ; i < lines - 12 ; i++){
+        path[i] = i ;
+       // covered[i] = 1 ;
+    }
+}
+
+void
+next_prefix(){
+    path[0] = 0 ;
+    covered[0] = 1 ;
+    _next_prefix(1) ;
+    covered[0] = 0;
+}
+
+void
+_next_prefix(int idx){ // prefix[0], prefix[1], ..., prefix[N-13]
+    int i ;
+    int last_prefix = lines -12 ;
+
+    while (idx < last_prefix) {
+        if(idx == last_prefix -1 ){ // when 0,1,2,3,4~15
+            
+            for(i = 0 ; i < lines ; i++){
+                if (covered[i] == 0 && path[idx] < i) {
+                    path[idx] = i ;
+                    covered[i] = 1 ;
+                    prefix_weight += route[path[idx-1]][i] ;
+                    if(i == lines - 1){ // when 0,1,2,3,15
+                        idx-- ;
+                        path[idx]++ ;
+                        covered[idx] =0 ;
+                        break ;
+                    }
+                    idx++ ;
+                    
+                    prefix_weight -= route[path[idx-1]][i] ;
+                    break ;
+                }
+            }
+        }
+        else{ // when 0,1,2,3,~
+            for (int i =0 ; i < lines ; i++) {
+                if (covered[i] == 0) {
+                    path[idx] = i ;
+                    covered[i] = 1 ;
+                    prefix_weight += route[path[idx-1]][i] ;
+                    idx++ ;
+                    break ;
+                }
+            }
+        }
+    }
+    prefix_weight -= route[path[idx-1]][i] ;
+    covered[i] = 0 ;
 
 }
 
 void
-_travel(int idx) { //professor's code
-	int i ;
-
-	if(idx == lines) {
-		tot_weight += route[path[lines-1]][path[0]] ;
-		if(min == -1L || min > tot_weight) {
-			min = tot_weight ; 
-
-			printf("%ld (", tot_weight) ;
-			for (i = 0 ; i < lines ; i++)
-				printf("%d ", path[i]) ;
-			printf("%d)\n", path[0]) ;
-
-		}
-		tot_weight -= route[path[lines-1]][path[0]] ;
-	}
-	else{
-		for (i = 0 ; i < lines ; i++) {
-			if (covered[i] == 0){
-				path[idx] = i ;
-				covered[i] = 1 ;
-				tot_weight += route[path[idx-1]][i] ;
-				_travel(idx+1) ;
-				tot_weight -= route[path[idx-1]][i] ;
-				covered[i] = 0 ;
-			}
-		}
-	}
-}
-
-void
-travel(int start) { // professor's code
-	path[0] = start ; 
-	covered[start] = 1 ;
-	_travel(1) ;
-	covered[start] = 0 ;
-}
-
-void
-spawning(int prefix) {
-	int pipes[2] ;
-
+spawning(int* prefix) {
+	int pipes[2] ; // 0 : reader / 1 : writer
+    pid_t child_pid ;
 	/* create unnamed pipes to communicate parent and children */
 	if (pipe(pipes) != 0 ){
 		perror("Create pipes Error! \n") ;
 		exit(1) ;
 	}
-	if((pid = fork()) == 0){ /* child */ 
-		children(prefix, p[0]) ;
+	if((child_pid = fork()) == 0){ /* child */
+		child_proc(prefix, pipes) ;
 	}
 	else{ /* parent */
 		close(p[1]) ;
-		current_child_info(pid, p[0]) ;
+		current_child_info(child_pid, p[0]) ;
 	}
 }
 
@@ -113,17 +131,16 @@ void
 current_child_info(pid_t pid, int *p){
 	
 }
+
 int
 main(int argc, char** argv){
 	// argv[0] = exe command , argv[1] = tsp file instance , argv[2] = number of children 
 
-	pid_t child[12] ; // Maximum 12 child processes should be available.
-	pid_t child_pid ;
-	int i = 0, j = 0; // variables for loops or something
-	int exit_code ;
-	int t = 0 ; 
+	pid_t pid ;
+	int i = 0, j = 0, t= 0; // variables for loops or something
+	int exit_code, children =0 ;
 	char f_input[1024] ;
-	// prefix variable necessary. But what type?? 
+
 
 	signal(SIGINT, parent_handler);
 	if(argc != 3){
@@ -131,10 +148,8 @@ main(int argc, char** argv){
 		exit(0);
 	}
 
-	printf("starting point. Master process : %d \n ", getpid()); //delete this later
-	FILE * fp = fopen(argv[1], "r");
-
-	/* To get a length of file instance */
+    /* To get a length of file instance */
+    FILE * fp = fopen(argv[1], "r");
 	while(!feof(fp)){
 		fgets(f_input, sizeof(f_input), fp) ;
 		lines++ ; 
@@ -154,37 +169,33 @@ main(int argc, char** argv){
 			route[i][j] = t ;
 		}
 	}
-
 	fclose(fp) ; 
 
+    init_prefix(route) ;
 
 	/* creating Children Processes */
-	while ( ){
-		if ( (child_pid =fork() ) < 0) { /* fork fail */
-			perror (" Fork failed ! \n") ;
-			exit(1) ;
-		}
-		else if ( child_pid == 0) { /* Children */
-			printf("Parent : %d , child[%d] : %d\n", getppid(), i, getpid() /* child[i] */ ) ;
-			// then I have to connect children with parent via pipe(write)
-			//exit(0) ;	
-		}
-		else { /* master(parent) process */
-			// using pipe read file operation, take the solution from children
-			//waitpid(child[i], NULL, 0) ; 
-			//wait(0x0) ;
-			printf("Process %d spawned process %d\n", getpid(), child_pid) ;
-		}
-		wait(0x0) ;
-	}
-
-	/* Deallocating the route table then terminate normally */
-	for(i = 0 ; i < lines ; i++) {	free(route[i]) ;  }
-	free(route) ;
-	printf(" The best solution is %ld \n", tot_weight );
-	printf(" And the number of covered routes are %llu \n", count );
-	printf("Matrix has been deallocated. Bye! \n");
-	exit(0) ;
-
+	while(path != end_prefix){
+        if ( children == argv[2] ) {
+            pid = wait() ;
+            children-- ;
+//            read_result_and update_best(pid) ; 
+        }
+        if (terminate == 0) {
+            spawning(path) ;
+            children++ ;
+            next(path) ;
+        }
+        else{ // when terminate is 1(True)
+            if (children == 0){
+                /* Deallocating the route table then terminate normally */
+                for(i = 0 ; i < lines ; i++) {    free(route[i]) ;  }
+                free(route) ;
+                printf(" The best solution is %ld \n", tot_weight );
+                printf(" And the number of covered routes are %llu \n", count );
+                printf("Matrix has been deallocated. Bye! \n");
+                exit(0) ;
+            }
+        }
+    }
 }
 
--- hw2/tsp17.c
+++ hw2/tsp17.c
@@ -10,13 +10,13 @@ int min = -1 ;
 
 void _travel(int idx) {
 	int i ;
-
+	//idx는 1부터 시작해서 재귀적으로 돈다.  
 	if (idx == 17) {
-		length += m[path[16]][path[0]] ;
+		length += m[path[16]][path[0]] ; // travel 함수에선 항상 idx를 0을 주는데. 
 		if (min == -1 || min > length) {
 			min = length ;
-
-			printf("%d (", length) ;
+			// 중복을 제외하고, 첫번째 인덱스를 기준으로 해서 출력이 될것.
+			printf("%d (", length) ; 
 			for (i = 0 ; i < 17 ; i++) 
 				printf("%d ", path[i]) ;
 			printf("%d)\n", path[0]) ;	
@@ -28,8 +28,8 @@ void _travel(int idx) {
 			if (used[i] == 0) {
 				path[idx] = i ;
 				used[i] = 1 ;
-				length += m[path[idx-1]][i] ;
-				_travel(idx+1) ;
+				length += m[path[idx-1]][i] ; 
+				_travel(idx+1) ; //여기서 재귀적으로 들어감. idx를 늘려줌
 				length -= m[path[idx-1]][i] ;
 				used[i] = 0 ;
 			}
